b22a7163f5bafa596872fd740c8986e5604aee6b
chetanya-shrimali <chetanyashrimalie5@gmail.com>
2017-10-12 15:05:04 +0530

    multiple fixes

--- music/urls.py
+++ music/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
-
 from music import views
+
+app_name = 'music'
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^(?P<record_id>[0-9]+)/$', views.record_detail, name='record_detail')
 ]
--- music/views.py
+++ music/views.py
@@ -1,7 +1,18 @@
 from __future__ import unicode_literals
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Record, Song
 
 
 def index(request):
-    return HttpResponse("Hey There")
+    records = Record.objects.all()
+    # template = loader.get_template('./music/index.html')
+    context = {
+        'records': records,
+    }
+    # return HttpResponse(template.render(context, request))
+    return render(request, './music/index.html', context)
+
+
+def record_detail(request, record_id):
+    record = get_object_or_404(Record, pk=record_id)
+    return render(request, './music/details.html', {'record': record})
