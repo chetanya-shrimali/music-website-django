555437f7d79157a67d12e76f35bd341f096061c8
chetanya-shrimali <chetanyashrimalie5@gmail.com>
2017-11-06 23:55:26 +0530

    added register and login features

--- music/urls.py
+++ music/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+
 from music import views
 
 app_name = 'music'
@@ -7,7 +8,9 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<pk>[0-9]+)/$', views.record_detail, name='record_detail'),
     url(r'^add/$', views.RecordCreate.as_view(), name='add-record'),
-    url(r'^(?P<pk>[0-9]+)/update/', views.RecordUpdate.as_view(), name='update-record'),
-    # url(r'^record/(?P<pk>[0-9]+)/delete/', views.RecordDelete.as_view(), name='delete-record'),
+    url(r'^(?P<pk>[0-9]+)/update/', views.RecordUpdate.as_view(),
+        name='update-record'),
+    # url(r'^record/(?P<pk>[0-9]+)/delete/', views.RecordDelete.as_view(),
+    # name='delete-record'),
     url(r'^register/$', views.UserFormView.as_view(), name='register')
 ]
